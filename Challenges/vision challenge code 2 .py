99187bb806de3e554c0b85c614f27694694edcf4
Sahil-L <63905606+Sahil-L@users.noreply.github.com>
2023-01-02 17:43:36 +0000

    Add files via upload

---
+++ Challenges/vision challenge code 2 .py
@@ -0,0 +1,46 @@
+from sr.robot3 import *
+import math
+import time
+
+R = Robot()
+
+def bulletPoint2():
+    
+    markers = R.camera.see()
+    if len(markers) != 0:
+        closestMarker = markers[0]
+        for marker in markers:
+            if marker.distance < closestMarker.distance:
+                closestMarker = marker
+
+    while True:
+
+        #camera sees and idenifies the closest Marker to it    
+        markers = R.camera.see()
+        if len(markers) != 0:
+            closestMarker = markers[0]
+            for marker in markers:
+                if marker.distance < closestMarker.distance:
+                    closestMarker = marker
+        
+        
+        #calculates an angle for the marker from radians into degree
+        angle = closestMarker.spherical.rot_y * (180/math.pi)
+        print(angle)
+        
+        #checks if angle is a certain value and lights up the correct LED accordingly
+        if angle < 60:
+            R.kch.leds[UserLED.A] = Colour.BLUE
+            R.kch.leds[UserLED.C] = Colour.OFF
+            R.kch.leds[UserLED.B] = Colour.OFF
+        elif angle > 120:
+            R.kch.leds[UserLED.C] = Colour.BLUE
+            R.kch.leds[UserLED.A] = Colour.OFF
+            R.kch.leds[UserLED.B] = Colour.OFF
+        else:
+            R.kch.leds[UserLED.B] = Colour.BLUE
+            R.kch.leds[UserLED.A] = Colour.OFF
+            R.kch.leds[UserLED.C] = Colour.OFF
+        time.sleep(0.5)
+
+bulletPoint2()
