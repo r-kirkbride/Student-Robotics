9ecc151166fbb90b2eeeb7625092fc8132380b88
r-kirkbride <114223050+r-kirkbride@users.noreply.github.com>
2023-01-11 13:18:32 +0000

    Update test1.py
    Now loops forever

--- Ruggeduino/test1.py
+++ Ruggeduino/test1.py
@@ -1,6 +1,7 @@
 from sr.robot3 import Robot as R
 
-left = R.ruggeduino.command("x")
-right = R.ruggeduino.command("y")
-print(f"Left Motor: {left} degrees")
-print(f"Right Motor: {right} degrees")
+while True:
+    left = R.ruggeduino.command("x")
+    right = R.ruggeduino.command("y")
+    print(f"Left Motor: {left} degrees")
+    print(f"Right Motor: {right} degrees")
