9b23e9542ad4c61f05ef923f86e0d608138df4a0
r-kirkbride <114223050+r-kirkbride@users.noreply.github.com>
2023-01-11 13:22:20 +0000

    Update test2.py
    loops rotating the motor by increasing numbers of rotations every 5 seconds

--- Ruggeduino/test2.py
+++ Ruggeduino/test2.py
@@ -1,3 +1,9 @@
-from sr.robot3 import Robot as R
+#from sr.robot3 import Robot as R
 import moveFunctions as move
+import time
 
+n=1
+while True:
+    move.moveRot(n,1)
+    time.sleep(5)
+    n += 1
