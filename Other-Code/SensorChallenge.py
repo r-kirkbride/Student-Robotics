5ea04098707d95321954ad77201cb59de2a815bb
r-kirkbride <114223050+r-kirkbride@users.noreply.github.com>
2023-03-01 10:53:27 +0000

    Update SensorChallenge.py

--- Other-Code/SensorChallenge.py
+++ Other-Code/SensorChallenge.py
@@ -1,22 +1,17 @@
 from sr.robot3 import *
-import cv2
-import numpy as np
 import time
+from opencv import cv2
+import numpy as np
+
 R = Robot()
 
 while True:
     markers = R.camera.see()
-    if len(markers)>0:
-        frame = R.camera.capture()
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        lower_val = np.array([20, 100, 100])
-        higher_val = np.array([30, 255, 255])
-        mask = cv2.inRange(hsv, lower_val, higher_val)
-        if np.sum(mask) > 0:
-            R.kch.leds[UserLED.C] = Colour.RED
-        else:
-            R.kch.leds[UserLED.A] = Colour.CYAN
-    
-    time.sleep(2)
-    R.kch.leds[UserLED.A] = Colour.OFF
-    R.kch.leds[UserLED.C] = Colour.OFF
+    if len(markers) >0:
+        frame = R.camera.capture
+        image = cv2.imread(frame)
+        original = image.copy()
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        lower = np.array([22, 93, 0], dtype="uint8")
+        upper = np.array([45, 255, 255], dtype="uint8")
+        mask = cv2.inRange(image, lower, upper)
